3c0810d0ca57e21acfe89c6c862ce3a8fa3f4bbd
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-10-25 18:38:43 +0600

    banner page

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
-import Media from './Media';
+import Media from "./Media";
 
 const LeftBanner = () => {
-    const [text] = useTypewriter({
-      words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
-      loop: true,
-      typeSpeed: 20,
-      deleteSpeed: 10,
-      delaySpeed: 2000,
-    });
+  const [text] = useTypewriter({
+    words: [
+      "MERN Stack Developer.",
+      "Front-End Developer.",
+      "Back-End Developer.",
+      "React / Next.js develoepr.",
+    ],
+    loop: true,
+    typeSpeed: 50,
+    deleteSpeed: 20,
+    delaySpeed: 2000,
+  });
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
-        <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">John doe</span>
+        <h1 className="text-5xl font-bold text-white">
+          Hi, I'm{" "}
+          <span className="text-designColor capitalize">Rijwan Jannat</span>
         </h1>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
@@ -26,15 +32,18 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-          I use animation as a third dimension by which to simplify experiences
-          and kuiding thro each and every interaction. I'm not adding motion
-          just to spruce things up, but doing it in ways that.
+          "As a MERN stack developer, I have the privilege of creating digital
+          wonders. My code serves as the brush, delicately crafting web
+          experiences that both innovate and inspire. Persistence is my guiding
+          light, for it's through dedication that I mold the future. I continue
+          to code, I continue to evolve, and with each line, my journey adds to
+          the ever-changing canvas of possibilities."
         </p>
       </div>
-     {/* Media */}
-     <Media />
+      {/* Media */}
+      <Media />
     </div>
   );
-}
+};
 
-export default LeftBanner
+export default LeftBanner;
