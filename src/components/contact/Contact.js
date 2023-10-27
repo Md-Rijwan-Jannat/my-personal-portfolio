1be435fbfa823f79b17bb4976f4f6ed7ec31258e
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-10-27 22:42:38 +0600

    contact page complete

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ContactLeft from "./ContactLeft";
 import { Title } from "../ui/Title/Title";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const [username, setUsername] = useState("");
@@ -11,13 +12,11 @@ const Contact = () => {
   const [errMsg, setErrMsg] = useState("");
   const [successMsg, setSuccessMsg] = useState("");
 
-  // ========== Email Validation start here ==============
   const emailValidation = () => {
     return String(email)
-      .toLocaleLowerCase()
+      .toLowerCase()
       .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
   };
-  // ========== Email Validation end here ================
 
   const handleSend = (e) => {
     e.preventDefault();
@@ -30,19 +29,45 @@ const Contact = () => {
     } else if (!emailValidation(email)) {
       setErrMsg("Give a valid Email!");
     } else if (subject === "") {
-      setErrMsg("Plese give your Subject!");
+      setErrMsg("Please give your Subject!");
     } else if (message === "") {
       setErrMsg("Message is required!");
     } else {
-      setSuccessMsg(
-        `Thank you dear ${username}, Your Messages has been sent Successfully!`
-      );
-      setErrMsg("");
-      setUsername("");
-      setPhoneNumber("");
-      setEmail("");
-      setSubject("");
-      setMessage("");
+      // Prepare the email data
+      const emailData = {
+        from_name: username,
+        phone: phoneNumber,
+        subject: subject,
+        message: message,
+        reply_to: email,
+      };
+
+      // Use the emailjs.sendForm method to send the email
+      emailjs
+        .sendForm(
+          "service_a83mwk4",
+          "template_sotaxch",
+          emailData,
+          "wTuP0_-qh1sVDIcjH"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            setErrMsg(""); // Clear any previous error message
+            setSuccessMsg(
+              `Thank you, ${username}! Your message has been sent successfully.`
+            );
+            setUsername("");
+            setPhoneNumber("");
+            setEmail("");
+            setSubject("");
+            setMessage("");
+          },
+          (error) => {
+            console.log(error.text);
+            setErrMsg("Something went wrong. Please try again.");
+          }
+        );
     }
   };
   return (
@@ -56,7 +81,7 @@ const Contact = () => {
       <div className="w-full">
         <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
           <ContactLeft />
-          <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
+          <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#051320] to-[#0a0b0c] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
             <form className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5">
               {errMsg && (
                 <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
@@ -77,9 +102,8 @@ const Contact = () => {
                     onChange={(e) => setUsername(e.target.value)}
                     value={username}
                     className={`${
-                      errMsg === "Username is required!" &&
-                      "outline-designColor"
-                    } contactInput`}
+                      errMsg === "Username is required!" && "outline-gray-800"
+                    } bg-gray-900 p-3 rounded focus:outline-none focus:border-b-4 focus:border-gray-700`}
                     type="text"
                   />
                 </div>
@@ -92,8 +116,8 @@ const Contact = () => {
                     value={phoneNumber}
                     className={`${
                       errMsg === "Phone number is required!" &&
-                      "outline-designColor"
-                    } contactInput`}
+                      "outline-gray-800"
+                    } bg-gray-900 p-3 rounded focus:outline-none focus:border-b-4 focus:border-gray-700`}
                     type="text"
                   />
                 </div>
@@ -106,9 +130,8 @@ const Contact = () => {
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
                   className={`${
-                    errMsg === "Please give your Email!" &&
-                    "outline-designColor"
-                  } contactInput`}
+                    errMsg === "Please give your Email!" && "outline-gray-800"
+                  } bg-gray-900 p-3 rounded focus:outline-none focus:border-b-4 focus:border-gray-700`}
                   type="email"
                 />
               </div>
@@ -120,9 +143,8 @@ const Contact = () => {
                   onChange={(e) => setSubject(e.target.value)}
                   value={subject}
                   className={`${
-                    errMsg === "Plese give your Subject!" &&
-                    "outline-designColor"
-                  } contactInput`}
+                    errMsg === "Plese give your Subject!" && "outline-gray-800"
+                  } bg-gray-900 p-3 rounded focus:outline-none focus:border-b-4 focus:border-gray-700`}
                   type="text"
                 />
               </div>
@@ -134,8 +156,8 @@ const Contact = () => {
                   onChange={(e) => setMessage(e.target.value)}
                   value={message}
                   className={`${
-                    errMsg === "Message is required!" && "outline-designColor"
-                  } contactTextArea`}
+                    errMsg === "Message is required!" && "outline-gray-800"
+                  } bg-gray-900 p-3 rounded focus:outline-none focus:border-b-4 focus:border-gray-700`}
                   cols="30"
                   rows="8"
                 ></textarea>
@@ -143,7 +165,7 @@ const Contact = () => {
               <div className="w-full">
                 <button
                   onClick={handleSend}
-                  className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent"
+                  className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] border-transparent"
                 >
                   Send Message
                 </button>
--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,47 +1,106 @@
-import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { contactImg } from "../../assets/index";
+import React, { useState } from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaInstagram,
+} from "react-icons/fa";
+import profile from "../../assets/images/profile.jpg";
 
 const ContactLeft = () => {
+  const [showFullText, setShowFullText] = useState(false);
+
+  const toggleTextVisibility = () => {
+    setShowFullText(!showFullText);
+  };
+
+  const text =
+    "I am a dedicated MERN Stack Developer with expertise in web development. I excel in building full-stack applications using MongoDB, Express, React, and Node.js, and I'm passionate about creating user-friendly, elegant web solutions. With a strong commitment to staying up-to-date with industry best practices, I'm ready to tackle new challenges and turn digital ideas into reality.";
+
+  const displayedText = showFullText ? text : text.slice(0, 200);
+
   return (
-    <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
-      <img
-        className="w-full h-64 object-cover rounded-lg mb-2"
-        src={contactImg}
-        alt="contactImg"
-      />
+    <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#0a0b0c] to-[#051320] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
+      <div className="relative">
+        <div>
+          <img
+            className="w-full h-64 object-cover rounded-lg mb-2"
+            src={"https://i.ibb.co/2NJrdGL/1698420881184-01.jpg"}
+            alt="contactImg"
+          />
+        </div>
+        <img
+          className="w-24 h-24 rounded-full object-cover ring-2 ring-gray-600 mb-2 absolute -bottom-7 left-3"
+          src={profile}
+          alt="contactImg"
+        />
+      </div>
+
       <div className="flex flex-col gap-4">
-        <h3 className="text-3xl font-bold text-white">John Doe</h3>
+        <h3 className="text-3xl font-bold text-white">MD Rijwan Jannat</h3>
         <p className="text-lg font-normal text-gray-400">
           MERN Stack Developer
         </p>
         <p className="text-base text-gray-400 tracking-wide">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-          ipsam autem cumque, accusantium dicta odio.
+          {displayedText}
+          {text.length > 200 && !showFullText && (
+            <span
+              className="text-lightText cursor-pointer"
+              onClick={toggleTextVisibility}
+            >
+              ...See More
+            </span>
+          )}
+          {showFullText && (
+            <span>
+              <div
+                className="text-lightText cursor-pointer"
+                onClick={toggleTextVisibility}
+              >
+                Hide...
+              </div>
+            </span>
+          )}
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Phone: <span className="text-lightText">+968 97859628</span>
+          Phone: <span className="text-lightText">(+880) 1797660947</span>
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Email: <span className="text-lightText">noor.jsdivs@gmail.com</span>
+          Email:{" "}
+          <span className="text-lightText">rijwanjannat36@gmail.com</span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
         <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
-          <span className="bannerIcon">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/profile.php?id=100086218014706"
+            className="bannerIcon"
+          >
             <FaFacebookF />
-          </span>
-          <span className="bannerIcon">
-            <FaTwitter />
-          </span>
-          <span className="bannerIcon">
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/rijwanjannat/"
+            className="bannerIcon"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/rijwanjannat/"
+            className="bannerIcon"
+          >
             <FaLinkedinIn />
-          </span>
+          </a>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default ContactLeft
+export default ContactLeft;
